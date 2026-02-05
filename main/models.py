d1828ac8bb8d172fde63b536e9a94843c59d1f58
MrWir4uck <matviiv.nazar2010@gmail.com>
2026-02-05 20:34:31 +0200

    Добавить модель Post и  Category зарегистрировать её в админке

--- main/models.py
+++ main/models.py
@@ -8,3 +8,23 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    image = models.ImageField(
+        upload_to='post_images/',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
